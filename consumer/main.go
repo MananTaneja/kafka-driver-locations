8a51a6e3ea6e7a62c5e03c6252c23938092651ab
MananTaneja <manantaneja007@gmail.com>
2026-02-01 15:09:58 +0530

    feat: setting up consumer and producer

--- consumer/main.go
+++ consumer/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/segmentio/kafka-go"
 )
 
@@ -8,9 +11,25 @@ func main() {
 	broker := "localhost:9092"
 
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{broker},
-		Topic:   "driver-location",
+		Brokers:     []string{broker},
+		Topic:       "driver-location",
+		Partition:   0,
+		StartOffset: kafka.LastOffset,
+		MinBytes:    10e3, // 10KB
+		MaxBytes:    10e6, // 10MB
 	})
 
 	defer reader.Close()
+
+	ctx := context.Background()
+
+	fmt.Println("Consuming a single message from Kafka...")
+	message, err := reader.ReadMessage(ctx)
+	if err != nil {
+		fmt.Printf("Failed to read message from Kafka: %v", err)
+	}
+
+	fmt.Printf("Message consumed: key=%s value=%s\n", string(message.Key), string(message.Value))
+
+	fmt.Println("Kafka message consumption complete. Exiting.")
 }
--- producer/main.go
+++ producer/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/segmentio/kafka-go"
 )
 
@@ -8,9 +12,29 @@ func main() {
 	brokerAddress := "localhost:9092"
 
 	writer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{brokerAddress},
-		Topic:   "driver-location",
+		Brokers:   []string{brokerAddress},
+		Topic:     "driver-location",
+		Balancer:  &kafka.LeastBytes{},
+		BatchSize: 1,
 	})
 
 	defer writer.Close()
+
+	message := kafka.Message{
+		Key:   []byte("driver-key"),
+		Value: []byte("12"),
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fmt.Println("Sending message to kafka")
+	err := writer.WriteMessages(ctx, message)
+
+	if err != nil {
+		fmt.Printf("error occured: %v", err)
+	}
+
+	fmt.Println("Message sent successfully.")
+	fmt.Println("Kafka producer has completed sending the message.")
 }
